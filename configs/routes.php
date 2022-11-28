a6185b4f7b91fad63d32c23a64d52962a91df4cd
Chijindu G. Nzeako <chijindunzeako517@gmail.com>
2022-11-28 03:35:57 +0100

    updated README file

--- configs/routes.php
+++ configs/routes.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file contains configuration settings for custom routes
+ * This file contains configuration settings for custom routes that your plugin will register.
  *
  * @author      Chijindu Nzeako <chijindunzeako517@gmail.com>
  * @license     https://www.gnu.org/licenses/agpl-3.0.txt GNU/AGPLv3
@@ -8,25 +8,36 @@
  */
 
 return array(
+
     /**
      * Set routes that need to be registered along with the views
      */
     'routes'    => array(
+
         /**
          * Add a route for {site_url}/wps-page
          *
          */
+
         '/wps-page'     => array(
+
             /**
-             * The view displayed by the route
+             * Specify the view displayed by the route
              */
             'view'  => 'pages.wps-custom-page',
 
             /**
-             * The page title
+             * Specify the page title
              */
             'title' => 'WPS Custom Page',
+
         ),
+
+        /**
+         * You can add your custom routes below
+         */
+
+
     ),
 
 );
