638251922c91bd64d71f637910b21c474a16ae04
Chijindu G. Nzeako <chijindunzeako517@gmail.com>
2023-04-12 12:37:40 +0100

    updated main plugin file
        updated plugin header comment section.

--- wordpress-plugin-starter.php
+++ wordpress-plugin-starter.php
@@ -1,36 +1,53 @@
 <?php
 /*
-Plugin Name: WPS Plugin
-Plugin URI:
-Description: A boiler-plate for wordpress plugin
-Version: 1.0.0
-Requires at least: 5.7
-Requires PHP: 8.0
-Author: Codestar Technologies
-Author URI: https://codestar.com.ng
-License: AGPLv3
-License URI:
-Text Domain: wps
-Domain Path: /languages
+Plugin Name:        WPS Plugin
+Plugin URI:         
+Description:        
+Version:            1.0.0
+Requires at least:  5.6.5
+Requires PHP:       8.0
+Author:             
+Author URI:         
+License:            AGPLv3
+License URI:        https://www.gnu.org/licenses/agpl-3.0.en.html
+Text Domain:        wps
+Domain Path:        /languages
 */
+
 /*
-{Plugin Name} is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-any later version.
+{Plugin Short Description}
+Copyright (C) {Year} {Organisation}
 
-{Plugin Name} is distributed in the hope that it will be useful,
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-You should have received a copy of the {License}
-along with {Plugin Name}. If not, see {License URI}.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/agpl-3.0.en.html.
 */
 
-/**
- * The full-path and file name of the plugin file
+
+/*
+ * You are free to change the header comment section to match information required by your theme. Items that can modified includes:
+ *
+ * 1. Plugin Name:      (required) The name of your plugin, which will be displayed in the Plugins list in the WordPress Admin.
+ * 2. Plugin URI:       The home page of the plugin, which should be a unique URL, preferably on your own website.
+ * 5. Description:      A short description of the plugin, as displayed in the Plugins section in the WordPress Admin.
+ * 7. Version:          The current version number of the plugin, such as 1.0 or 1.0.3.
+ * 3. Author:           The name of the plugin author. Multiple authors may be listed using commas.
+ * 4. Author URI:       The author’s website or profile on another website, such as WordPress.org.
+ * 8. Text Domain:      The gettext text domain of the plugin.
+ *
  */
+
+
+// The full-path and file name of the plugin file
 if ( ! defined( 'WPS_FILE' ) ) {
     define( 'WPS_FILE', __FILE__ );
 }
